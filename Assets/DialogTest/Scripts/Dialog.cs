af42580057b6870e926337aa1bfc6178f6d97baa
DESKTOP-52351T8\Admin <yojhf@naver.com>
2025-01-02 13:13:36 +0900

    no message

---
+++ Assets/DialogTest/Scripts/Dialog.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MySampleEx
+{
+    [Serializable]
+    public class Dialog
+    {
+        public int number;
+        public int character;
+        public string name;
+        public string sentence;
+
+    }
+}
